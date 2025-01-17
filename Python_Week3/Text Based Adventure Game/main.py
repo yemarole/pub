1cf7f511b81097f5c406494d7451d54f068acba0
yemarole <yemiarole@icloud.com>
2025-01-17 09:30:52 +0000

    updated

--- Python_Week3/Text Based Adventure Game/main.py
+++ Python_Week3/Text Based Adventure Game/main.py
@@ -3,3 +3,7 @@ from room import Room
 kitchen = Room("kitchen")
 ballroom = Room("ballroom")
 dining_hall = Room("dining hall")
+
+kitchen.set_description("A dank and dirty room buzzing with flies.")
+
+print(kitchen.describe())
--- Python_Week3/Text Based Adventure Game/room.py
+++ Python_Week3/Text Based Adventure Game/room.py
@@ -2,3 +2,18 @@ class Room:
     def __init__(self, room_name):
         self.name = room_name
         self.description = None
+
+    def get_description(self):
+        return self.description
+
+    def set_description(self, room_description):
+        self.description = room_description
+
+    def describe(self):
+        print(self.description)
+
+    def set_name(self, room_name):
+        self.name = room_name
+
+    def get_name(self):
+        return self.name
